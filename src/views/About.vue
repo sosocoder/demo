bb8b3af66fb7b59880268dea874a989a2be4ffbc
soso <408248960@qq.com>
2020-08-03 22:15:53 +0800

    面试demo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>谢谢观看</h1>
   </div>
 </template>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,99 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="box" id="box" >
+      <div class="imgBox" v-for="(item,index) in imglists" :key="index" @mouseover='todo(item)' @mouseout='todo1(item)'>
+        <img :src='item.url'>
+        <div class="info" v-show="item.flag">
+          <span>{{item.title}}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      imglists:[
+      {url:require("../imgs/1.jpg"),title:'图片一',flag:false},
+      {url:require("../imgs/2.jpg"),title:'图片二',flag:false},
+      {url:require("../imgs/3.jpg"),title:'图片三',flag:false},
+      {url:require("../imgs/4.jpg"),title:'图片四',flag:false},
+      {url:require("../imgs/5.jpg"),title:'图片五',flag:false},
+      {url:require("../imgs/6.jpg"),title:'图片六',flag:false},
+      {url:require("../imgs/7.jpg"),title:'图片四',flag:false},
+      {url:require("../imgs/8.jpg"),title:'图片五',flag:false},
+      {url:require("../imgs/9.jpg"),title:'图片六',flag:false},
+      {url:require("../imgs/5.jpg"),title:'图片五',flag:false},
+      {url:require("../imgs/6.jpg"),title:'图片六',flag:false},
+      {url:require("../imgs/7.jpg"),title:'图片四',flag:false},
+      {url:require("../imgs/8.jpg"),title:'图片五',flag:false},
+      {url:require("../imgs/9.jpg"),title:'图片六',flag:false},
+      ]
+    };
+  },
+  methods:{
+    todo(i){
+      i.flag = !i.flag
+    },
+    todo1(i){
+      i.flag = !i.flag
+    }
   }
-}
+};
 </script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.box {
+  width: 1500px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+}
+.box:after {
+  /* 处理不能填充满一行的空白区域不被已有图片平分*/
+  content: "";
+  flex-grow: 99999;
+}
+.imgBox {
+  flex-grow: 1;
+  margin: 5px;
+  position: relative;
+  /* overflow: hidden; */
+   
+}
+.imgBox:hover .info{
+  opacity: 1;
+}
+.imgBox .info{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  bottom: 5px;
+  opacity: 0;
+  transition: all 0.3s linear;
+}
+.imgBox .info:hover{
+  opacity: 0.8;
+  transform: translateX(40%);
+}
+.imgBox .info span{
+  font-weight: bolder;
+  color: #fff;
+  position: absolute;
+  bottom: 0;
+  display: block;
+}
+.imgBox img {
+  width: auto;
+  height: 150px;
+  object-fit: cover;
+  min-width: 100%;
+}
+</style>
